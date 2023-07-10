fd123cb92fd86960dec5e9fe6a8fea487039c7ef
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-07-10 16:09:17 +0530

    updated adminuser

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { SignUp, SignIn, UpdateUser } = require('../middlewares/adminUser.js')
+const { SignUp, SignIn, UpdateUser, GetUser } = require('../middlewares/adminUser.js')
 
 
 const router = Router()
@@ -7,6 +7,8 @@ const router = Router()
 router.post("/signup",SignUp)
 router.post("/signin",SignIn)
 router.put("/user/:phone",UpdateUser)
+router.get("/user/:phone",GetUser)
+
 
 
 
